d8aba490f94bba886cc8cf52750b4f123f618781
M1KK3R <tarmo.unt@gmail.com>
2021-10-07 20:14:39 +0300

    Kolmanda päeva lõpp

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  esemed = [
+    {pealkiri: 'ese1', hind: '100 €'},
+    {pealkiri: 'ese2', hind: '200 €'},
+    {pealkiri: 'ese3', hind: '400 €'}
+  ];
+  lisatud = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ostukorviLisamine(){
+    console.log("töötab");
+    console.log(this.esemed);
+    this.esemed[0].hind = "150 €";
+    this.lisatud = true;
+    
   }
 
 }
